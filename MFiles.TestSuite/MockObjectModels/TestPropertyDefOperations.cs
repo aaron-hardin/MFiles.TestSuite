199196233951641a8c6d949c0da74bfe3b2de43d
aaron-hardin <teslasprotege@gmail.com>
2014-08-04 12:49:57 -0500

    updated pdef ops for metrics

--- MFiles.TestSuite/MockObjectModels/TestPropertyDefOperations.cs
+++ MFiles.TestSuite/MockObjectModels/TestPropertyDefOperations.cs
@@ -4,84 +4,108 @@ using MFilesAPI;
 
 namespace MFiles.TestSuite.MockObjectModels
 {
-    public class TestPropertyDefOperations : VaultPropertyDefOperations
-    {
-        private TestVault vault;
+	public class TestPropertyDefOperations : VaultPropertyDefOperations
+	{
+		private TestVault vault;
 
-        public TestPropertyDefOperations(TestVault vault)
-        {
-            this.vault = vault;
-        }
+		public TestPropertyDefOperations( TestVault vault )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public PropertyDefAdmin AddPropertyDefAdmin(PropertyDefAdmin PropertyDefAdmin, TypedValue ResetLastUsedValue = null)
-        {
-            // TODO: use arguments
-            // TODO: error checking
-            this.vault.propertyDefs.Add(PropertyDefAdmin);
-            return PropertyDefAdmin;
-        }
+			this.vault = vault;
+		}
 
-        public PropertyDef GetBuiltInPropertyDef(MFBuiltInPropertyDef PropertyDefType)
-        {
-            throw new NotImplementedException();
-        }
+		public PropertyDefAdmin AddPropertyDefAdmin( PropertyDefAdmin propertyDefAdmin, TypedValue resetLastUsedValue = null )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public PropertyDef GetPropertyDef(int PropertyDef)
-        {
-            // TODO: verify functionality
-            if(PropertyDef < 0)
-                throw new IndexOutOfRangeException("ID out of range");
-            PropertyDefAdmin pda = this.vault.propertyDefs.SingleOrDefault(pdef => pdef.PropertyDef.ID == PropertyDef);
-            return pda == null ? null : pda.PropertyDef;
-        }
+			// TODO: use arguments
+			// TODO: error checking
+			vault.propertyDefs.Add( propertyDefAdmin );
+			return propertyDefAdmin;
+		}
 
-        public PropertyDefAdmin GetPropertyDefAdmin(int PropertyDef)
-        {
-            throw new NotImplementedException();
-        }
+		public PropertyDef GetBuiltInPropertyDef( MFBuiltInPropertyDef propertyDefType )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public int GetPropertyDefIDByAlias(string Alias)
-        {
-            try
-            {
-                // TODO: check all properties that use alias, following does not work
-                //return this.vault.propertyDefs.Single(pdef => pdef.SemanticAliases.Value.Contains(Alias)).PropertyDef.ID;
-                return this.vault.propertyDefs.Single(pdef => pdef.SemanticAliases.Value.Split(';').Contains(Alias)).PropertyDef.ID;
-            }
-            catch
-            {
-                return -1;
-            }
-        }
+			throw new NotImplementedException();
+		}
 
-        public int GetPropertyDefIDByGUID(string PropertyDefGUID)
-        {
-            throw new NotImplementedException();
-        }
+		public PropertyDef GetPropertyDef( int propertyDef )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public PropertyDefs GetPropertyDefs()
-        {
-            throw new NotImplementedException();
-        }
+			// TODO: verify functionality
+			if( propertyDef < 0 )
+				throw new IndexOutOfRangeException( "ID out of range" );
+			PropertyDefAdmin pda = vault.propertyDefs.SingleOrDefault( pdef => pdef.PropertyDef.ID == propertyDef );
+			return pda == null ? null : pda.PropertyDef;
+		}
 
-        public PropertyDefsAdmin GetPropertyDefsAdmin()
-        {
-            throw new NotImplementedException();
-        }
+		public PropertyDefAdmin GetPropertyDefAdmin( int propertyDef )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public void RemovePropertyDefAdmin(int PropertyDef, bool CopyToAnotherPropertyDef = false, int TargetPropertyDef = 0, bool Append = false, bool DeleteFromClassesIfNecessary = false)
-        {
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        public void UpdatePropertyDefAdmin(PropertyDefAdmin PropertyDefAdmin, TypedValue ResetLastUsedValue = null)
-        {
-            throw new NotImplementedException();
-        }
+		public int GetPropertyDefIDByAlias( string alias )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public void UpdatePropertyDefWithAutomaticPermissionsAdmin(PropertyDefAdmin PropertyDefAdmin, TypedValue ResetLastUsedValue = null, bool AutomaticPermissionsForcedActive = false)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			try
+			{
+				// TODO: check all properties that use alias, following does not work
+				//return this.vault.propertyDefs.Single(pdef => pdef.SemanticAliases.Value.Contains(Alias)).PropertyDef.ID;
+				return vault.propertyDefs.Single( pdef => pdef.SemanticAliases.Value.Split( ';' ).Contains( alias ) ).PropertyDef.ID;
+			}
+			catch
+			{
+				return -1;
+			}
+		}
+
+		public int GetPropertyDefIDByGUID( string propertyDefGuid )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public PropertyDefs GetPropertyDefs()
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public PropertyDefsAdmin GetPropertyDefsAdmin()
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public void RemovePropertyDefAdmin( int propertyDef, bool copyToAnotherPropertyDef = false, int targetPropertyDef = 0, bool append = false, bool deleteFromClassesIfNecessary = false )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public void UpdatePropertyDefAdmin( PropertyDefAdmin propertyDefAdmin, TypedValue resetLastUsedValue = null )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public void UpdatePropertyDefWithAutomaticPermissionsAdmin( PropertyDefAdmin propertyDefAdmin, TypedValue resetLastUsedValue = null, bool automaticPermissionsForcedActive = false )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+	}
 }
