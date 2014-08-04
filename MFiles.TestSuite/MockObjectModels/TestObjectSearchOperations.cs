a2c638d39186d9530539fddab7f0aec0d9d0313a
aaron-hardin <teslasprotege@gmail.com>
2014-08-04 12:47:00 -0500

    updated search ops for metrics

--- MFiles.TestSuite/MockObjectModels/TestObjectSearchOperations.cs
+++ MFiles.TestSuite/MockObjectModels/TestObjectSearchOperations.cs
@@ -3,73 +3,97 @@ using MFilesAPI;
 
 namespace MFiles.TestSuite.MockObjectModels
 {
-    public class TestObjectSearchOperations : VaultObjectSearchOperations
-    {
-        private TestVault vault;
+	public class TestObjectSearchOperations : VaultObjectSearchOperations
+	{
+		private TestVault vault;
 
-        public TestObjectSearchOperations(TestVault vault)
-        {
-            this.vault = vault;
-        }
+		public TestObjectSearchOperations( TestVault vault )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectVersionFile FindFile(string RelativePath, bool UpdateFromServer = false)
-        {
-            throw new NotImplementedException();
-        }
+			this.vault = vault;
+		}
 
-        public ObjectVersionAndProperties FindObjectVersionAndProperties(string RelativePath, bool UpdateFromServer = false)
-        {
-            throw new NotImplementedException();
-        }
+		public ObjectVersionFile FindFile( string relativePath, bool updateFromServer = false )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectSearchResults GetObjectsInPath(string RelativePath, bool SortResults, bool UpdateFromServer = false)
-        {
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        public bool IsObjectPathInMFiles(string RelativePath)
-        {
-            throw new NotImplementedException();
-        }
+		public ObjectVersionAndProperties FindObjectVersionAndProperties( string relativePath, bool updateFromServer = false )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectSearchResults SearchForObjectsByCondition(SearchCondition SearchCondition, bool SortResults)
-        {
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        public ObjectSearchResults SearchForObjectsByConditions(SearchConditions SearchConditions, MFSearchFlags SearchFlags, bool SortResults)
-        {
-            throw new NotImplementedException();
-        }
+		public ObjectSearchResults GetObjectsInPath( string relativePath, bool sortResults, bool updateFromServer = false )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectSearchResults SearchForObjectsByConditionsEx(SearchConditions SearchConditions, MFSearchFlags SearchFlags, bool SortResults, int MaxResultCount = 500, int SearchTimeoutInSeconds = 60)
-        {
-            throw new NotImplementedException();
+			throw new NotImplementedException();
+		}
 
-            //if(SearchFlags != MFSearchFlags.MFSearchFlagNone)
-            //    throw new NotImplementedException();
+		public bool IsObjectPathInMFiles( string relativePath )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-            //List<ObjectVersionAndProperties> objects = vault.ovaps.Where()
-        }
+			throw new NotImplementedException();
+		}
 
-        public XMLSearchResult SearchForObjectsByConditionsXML(SearchConditions SearchConditions, bool SortResults)
-        {
-            throw new NotImplementedException();
-        }
+		public ObjectSearchResults SearchForObjectsByCondition( SearchCondition searchCondition, bool sortResults )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectSearchResults SearchForObjectsByExportedSearchConditions(string ExportedSearchString, bool SortResults)
-        {
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        public XMLSearchResult SearchForObjectsByExportedSearchConditionsXML(string SearchString, bool SortResults)
-        {
-            throw new NotImplementedException();
-        }
+		public ObjectSearchResults SearchForObjectsByConditions( SearchConditions searchConditions, MFSearchFlags searchFlags, bool sortResults )
+		{
+			vault.MetricGatherer.MethodCalled();
 
-        public ObjectSearchResults SearchForObjectsByString(string SearchString, bool SortResults, MFFullTextSearchFlags FullTextSearchFlags)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			throw new NotImplementedException();
+		}
+
+		public ObjectSearchResults SearchForObjectsByConditionsEx( SearchConditions searchConditions, MFSearchFlags searchFlags, bool sortResults, int maxResultCount = 500, int searchTimeoutInSeconds = 60 )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+
+			//if(SearchFlags != MFSearchFlags.MFSearchFlagNone)
+			//    throw new NotImplementedException();
+
+			//List<ObjectVersionAndProperties> objects = vault.ovaps.Where()
+		}
+
+		public XMLSearchResult SearchForObjectsByConditionsXML( SearchConditions searchConditions, bool sortResults )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public ObjectSearchResults SearchForObjectsByExportedSearchConditions( string exportedSearchString, bool sortResults )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public XMLSearchResult SearchForObjectsByExportedSearchConditionsXML( string searchString, bool sortResults )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+
+		public ObjectSearchResults SearchForObjectsByString( string searchString, bool sortResults, MFFullTextSearchFlags fullTextSearchFlags )
+		{
+			vault.MetricGatherer.MethodCalled();
+
+			throw new NotImplementedException();
+		}
+	}
 }
